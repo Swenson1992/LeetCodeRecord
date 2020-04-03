5f502267f5113142a25ad8f67d70c3f4209c138f
宋健 <it.song925@gmail.com>
2020-04-03 18:41:16 +0800

    feat(medium): 8新思路

--- medium/8.字符串转换整数-atoi.js
+++ medium/8.字符串转换整数-atoi.js
@@ -9,64 +9,21 @@
  * @param {string} str
  * @return {number}
  */
-var myAtoi = function(str) {
-    let result = 0
-    str = str.trim()
-    if (str == "" || str == null) return result
-    let arr = str.split("");
-    let resultArr = []
+// 
+var myAtoi = function (str) {
+    const number = parseInt(str, 10);
+    const Max = Math.pow(2, 31) - 1;
+    const Min = Math.pow(-2, 31);
     
-    console.log(arr)
-    if (arr[0] == '+' || arr[0] == '-' || !isNaN(arr[0])) {
-        resultArr.push(arr[0])
-    } else {
-        return result
+    // 无法转换的情况返回 0
+    if (isNaN(number)) {
+        return 0;
     }
-    for(let i = 1; i < arr.length;i++) {
-        if (arr[i] == " ") {
-            break
-        }
-        if (!isNaN(arr[i]) || (arr[i] == '.' && !resultArr.includes('.'))) {
-            resultArr.push(arr[i])
-        } else {
-            break
-        }
+    // 转换结果超出范围的情况
+    if (number < Min || number > Max) {
+        return number < 0 ? Min : Max;
     }
-    console.log(resultArr)
-    if (resultArr.length == 0) {
-        return result
-    }
-    if (resultArr.length == 1) {
-        if (['+', '-'].includes(resultArr[0])) {
-            return result
-        } else {
-            return parseFloat(resultArr[0])
-        }
-    }
-    if (resultArr.length > 1) {
-        if (resultArr[0] == '+') {
-            resultArr.shift()
-            result = parseFloat(resultArr.join(""))
-            if (result > Math.pow(2,31) - 1) {
-                return Math.pow(2,31) - 1
-            }
-            return result
-        } else if (resultArr[0] == '-') {
-            resultArr.shift()
-            result = -1 * parseFloat(resultArr.join(""))
-            if (result < Math.pow(-2,31)) {
-                return Math.pow(-2,31)
-            }
-            return result
-        } else {
-            console.log(resultArr.join(""))
-            result = parseFloat(resultArr.join(""))
-            if (result > Math.pow(2,31) - 1) {
-                return Math.pow(2,31) - 1
-            }
-            return result
-        }
-    }
+    return number;
 };
 // @lc code=end
 
