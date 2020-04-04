dcca269996152e538fd9374816a25b1c86459e6e
一介俗人 <it.song925@gmail.com>
2020-04-04 18:06:57 +0800

    feat(hard): 42解题

---
+++ hard/42. 接雨水.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let max = 0;
+    let result = 0;
+    let leftMax = [], rightMax = [];
+    for(let i = 0; i < height.length; i++) {
+        leftMax[i] = max = Math.max(height[i], max)
+    }
+    max = 0
+    for (let i = height.length - 1; i >= 0; i--) {
+        rightMax[i] = max = Math.max(height[i], max)
+    }
+
+    for(let i = 0; i < height.length; i++) {
+        result = result + Math.min(leftMax[i], rightMax[i]) - height[i]
+    }
+    return result
+};
